facb23b8d42a4f61ab2dceb2b66e54927718f733
Joona Keskitalo <joona.keskitalo@gmail.com>
2026-07-01 07:30:17 +0300

    feat: show toast for longer duration

--- src/utils.js
+++ src/utils.js
@@ -85,5 +85,5 @@ export function showToast(message) {
   const toast = document.getElementById("toast");
   toast.textContent = message;
   toast.style.display = "block";
-  setTimeout(() => (toast.style.display = "none"), 2500);
+  setTimeout(() => (toast.style.display = "none"), 4000);
 }
